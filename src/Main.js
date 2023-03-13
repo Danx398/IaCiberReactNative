ef8d847aac36be0659c0d7e4ce255d11f003ddef
JossRV <jossrv74@gmail.com>
2023-03-13 13:44:46 -0600

    avance boton y titulo

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,47 @@
 import React from "react";
-import { View,Text } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
 
-export function Main (){
+export function Main() {
     return (
-        <View>
-            <Text>Hola mundo</Text>
+        <View style={styles.containertext}>
+            <Text style={styles.baseText}>Aplicación Para Encontrar malwares</Text>
+            <TouchableOpacity
+                style={styles.boton}
+                onPress={() => Alert.alert('Escaneando Información...')}
+            >
+                <View style={styles.containertext}>
+                    <Text style={styles.textoCentrado}>
+                        Escanear Archivos
+                    </Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    baseText: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        marginBottom: 20,
+    },
+    containertext: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    boton: {
+        // backgroundColor: '#B4E4FF',
+        backgroundColor: '#3E54AC',
+        width: 250,
+        height: 250,
+        borderRadius: 125
+    },
+    textoCentrado: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 40
+    }
+});
+
 // export default Main
