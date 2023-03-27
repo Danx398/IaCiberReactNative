c9af77423695d6b3aa8cc76ca77924bbff39b20b
danx398 <danidannniel398@gmail.com>
2023-03-26 19:26:46 -0600

    changed

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { View,Text } from "react-native";
+import { View, Text } from "react-native";
 
-export function Main (){
+export function Main() {
     return (
         <View>
-            <Text>Hola mundo</Text>
+            <Text style={styles.baseText}>Aplicación Para Encontrar malwares</Text>
+            <Button
+                onPress={() => Alert.alert('Escaneando Información...')}
+                title="Escaner Archivos"
+                accessibilityLabel="Learn more about this purple button"
+                color='red'
+            />
         </View>
     );
 }
+const styles = StyleSheet.create({
+    baseText: {
+      fontWeight: 'bold',
+      fontSize: 20,
+      marginBottom:20,
+    },
+    container: {
+      marginTop:350,
+      alignItems: 'center',
+      /*  flex: 1,
+       justifyContent: 'center', */
+    },
+  });
+  
 // export default Main
--- src/Prueba.js
+++ src/Prueba.js
@@ -1,11 +1,15 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
-export const Prueba = () =>{
-    return(
+export const Prueba = () => {
+    return (
         <View>
-            <Text>
-                Hola Danny
-            </Text>
+            <Text style={styles.baseText}>Aplicación Para Encontrar malwares</Text>
+            <Button
+                onPress={() => Alert.alert('Escaneando Información...')}
+                title="Escaner Archivos"
+                accessibilityLabel="Learn more about this purple button"
+                color='red'
+            />
         </View>
     );
 }
